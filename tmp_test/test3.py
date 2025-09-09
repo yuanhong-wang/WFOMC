5bb9ed398b61598d9a6eb06906004e886e6ca4cb
SunShixin <70331428+SunShixin6@users.noreply.github.com>
2025-09-09 20:03:57 +0800

    update

--- tmp_test/test3.py
+++
@@ -1,6 +0,0 @@
-from zoneinfo import ZoneInfo
-from datetime import datetime
-
-start_time = datetime.now(ZoneInfo("Asia/Shanghai"))
-print("代码运行时间戳：", start_time.strftime("%Y-%m-%d %H:%M:%S"))
-print("代码运行时间戳：", start_time.strftime("%Y%m%d_%H%M%S"))
