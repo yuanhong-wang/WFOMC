c1d58cd6a7a397685ae9dc9d304bfebf80879d99
lucienwang1009 <lucienwang@buaa.edu.cn>
2026-03-09 16:32:55 +0800

    feat(cython): update algo/__init__.py with try-import for Cython extensions

--- src/wfomc/algo/__init__.py
+++ src/wfomc/algo/__init__.py
@@ -1,15 +1,22 @@
 from enum import Enum
 
-from .StandardWFOMC import standard_wfomc
-from .FastWFOMC import fast_wfomc
-from .IncrementalWFOMC import incremental_wfomc
-from .RecursiveWFOMC import recursive_wfomc
+from wfomc._compat import try_import_cython as _try_cy
+
+_std = _try_cy("wfomc.algo.StandardWFOMC", "wfomc.algo.StandardWFOMC")
+_fast = _try_cy("wfomc.algo.FastWFOMC", "wfomc.algo.FastWFOMC")
+_incr = _try_cy("wfomc.algo.IncrementalWFOMC", "wfomc.algo.IncrementalWFOMC")
+_rec = _try_cy("wfomc.algo.RecursiveWFOMC", "wfomc.algo.RecursiveWFOMC")
+
+standard_wfomc = _std.standard_wfomc
+fast_wfomc = _fast.fast_wfomc
+incremental_wfomc = _incr.incremental_wfomc
+recursive_wfomc = _rec.recursive_wfomc
 
 __all__ = [
     "standard_wfomc",
     "fast_wfomc",
     "incremental_wfomc",
-    "recursive_wfomc"
+    "recursive_wfomc",
 ]
 
 
